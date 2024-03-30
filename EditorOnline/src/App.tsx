bb3d94e63488968524009f22dbd453afb05ac526
Maximo Mora <maxmora401@gmail.com>
2024-03-30 18:15:37 -0400

    componets

--- EditorOnline/src/App.tsx
+++ EditorOnline/src/App.tsx
@@ -16,10 +16,10 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Kevin + Kaku</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          Mena is {count}
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
